f289900eb38ef1ebed7cce7d64c6ec5827c77979
HHHHH-Y <984495875@qq.com>
2020-04-07 15:22:15 +0800

    2020/4/7写入

---
+++ Java基础语法/数据类型与运算符/HelloWorld.java
@@ -0,0 +1,39 @@
+
+// class A{
+
+/*
+}
+ */
+
+/**
+ * 文档注释
+ * 作者: HuYue
+ * 日期: 2020/4/7
+ * 描述: 如果一个类是public的, 那么这个类的类名一定要和文件名相同.
+ *       并且一个java文件中只能有一个public类
+ */
+
+public class HelloWorld{
+	public static void main(String args[]){
+		/*
+		整型int :  4个字节
+		标识符: 字母, 数字, 下划线, $
+		 */
+		int a2 = 0;
+		System.out.println(a);
+		// int a = 0;
+		// System.out.println(a);
+		// int max = Integer.MAX_VALUE + 1;
+		// System.out.println(max); // -2147483648
+		// int min = Integer.MIN_VALUE - 1;
+		// System.out.println(min); // 2147483647
+		// int a = 10;
+		// System.out.println(Integer.MAX_VALUE);  // 2147483647
+		// System.out.println(Integer.MIN_VALUE);  // -2147483648
+	 	//System.out.println(a); // 打印, 且换行
+	 	// System.out.print(a);  // 打印, 但是不换行
+	 	// System.out.printf("%d\n", a); // 以格式化的形式输出内容
+		//System.out.println("Hello, World");
+	}
+}
+
